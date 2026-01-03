881952c70f5b84b0b6419a8c6b78f86d51474ac7
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-01-03 08:15:12 -0300

    build: install and configure the Vite PostCSS plugin
    - The plugin instance was implemented with default settings
    
    - The jsconfig.json file was created to inform VS Code that the project is pure JavaScript, disabling type checking and eliminating warnings

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import postcss from '@vituum/vite-plugin-postcss';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), postcss()],
+
   server: {
     port: 3000,
     open: true,
