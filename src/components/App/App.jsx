a277e4ead9095c21a2f8ae98623bea467959c56d
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-01-08 09:14:00 -0300

    fix(App): correct HTML tag
    Change from <main> to <div>. The main tag will be implemented later to configure the page body.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,11 +5,11 @@ import './App.css';
 
 function App() {
   return (
-    <main className="page">
+    <div className="page">
       <NothingFound />
       <Preloader />
       <About />
-    </main>
+    </div>
   );
 }
 
