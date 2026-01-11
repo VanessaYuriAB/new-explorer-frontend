186ddc4cd467a6f3c5034b64334d2ca0b5d657c0
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-01-11 08:18:36 -0300

    feat(Navigation): add logged-out version for Navigation
    - JSX and CSS
    
    - Updated App component for temporary component preview
    
    - Implemented with loggedIn state variable via AuthContext context

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,27 @@
-/* import Header from '../Header/Header';
-import Main from '../Main/Main'; */
-import SavedNewsHeader from '../SavedNewsHeader/SavedNewsHeader';
-import SavedNewsCardList from '../SavedNewsCardList/SavedNewsCardList';
-import Preloader from '../Preloader/Preloader';
-import NothingFound from '../NothingFound/NothingFound';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
 import About from '../About/About';
+import AuthContext from '../../contexts/AuthContext';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // Variável de estado: status de login
+  const [loggedIn /*, setLoggedIn*/] = useState(false);
+
   return (
-    <div className="page">
-      {/* <Header />
-      <Main /> */}
-      <SavedNewsHeader />
-      <SavedNewsCardList />
-      <Preloader />
-      <NothingFound />
-      <About />
-    </div>
+    // Provedor de contexto: compartilha dados de login e do usuário atual
+    <AuthContext.Provider
+      value={{
+        loggedIn, // booleano de estado: status de login
+      }}
+    >
+      <div className="page">
+        <Header />
+        <Main />
+        <About />
+      </div>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/Header/components/Navigation/Navigation.jsx
+++ src/components/Header/components/Navigation/Navigation.jsx
@@ -1,36 +1,65 @@
 import './Navigation.css';
 import { NavLink, Link } from 'react-router-dom';
+import { useContext } from 'react';
+import AuthContext from '../../../../contexts/AuthContext';
 import btnOut from '../../../../assets/btn-out.svg';
 
 function Navigation() {
-  // A função getNavLinkClass é nativa do componente <NavLink>
-  // Aceita um objeto como um parâmetro, que possui uma propriedade,
-  // isActive, que é verdadeira se o link estiver ativo
+  // Contexto de autenticação, extraindo estado de login
+  const { loggedIn } = useContext(AuthContext);
+
+  // As funções getNavLinkClass e getNavLinkClassOut são nativas do componente <NavLink>
+  // Aceitam um objeto como parâmetro, que possui uma propriedade, isActive, que é
+  // verdadeira se o link estiver ativo
   const getNavLinkClass = ({ isActive }) => {
     return 'header__link' + (isActive ? ' header__link_active' : '');
   };
 
+  const getNavLinkClassOut = ({ isActive }) => {
+    return (
+      'header__link header__link_out' +
+      (isActive ? ' header__link_out_active' : '')
+    );
+  };
+
   return (
     <nav className="header__nav">
-      {/* Se o link estiver ativo, a classe header__link_active será adicionada à lista de
-      classes. */}
+      {/* Se o link estiver ativo, as classes header__link_active e header__link_out_active
+      serão adicionadas às listas de classes correspondentes */}
 
-      <div className="header__links">
-        <NavLink className={getNavLinkClass} to="/">
-          Início
-        </NavLink>
-        <NavLink className={getNavLinkClass} to="/saved-news">
-          Artigos salvos
-        </NavLink>
-      </div>
-      <Link className="header__btn" to="/">
-        <p className="header__btn-text">Nome</p>
-        <img
-          className="header__btn-out"
-          src={btnOut}
-          alt="Ícone simbolizando saída/logout."
-        />
-      </Link>
+      {/* Se estiver logado, renderiza Navigation; se não, renderiza Navigation _out */}
+
+      {loggedIn ? (
+        <>
+          <div className="header__links">
+            <NavLink className={getNavLinkClass} to="/">
+              Início
+            </NavLink>
+            <NavLink className={getNavLinkClass} to="/saved-news">
+              Artigos salvos
+            </NavLink>
+          </div>
+          <Link className="header__btn" to="/">
+            <p className="header__btn-text">Nome</p>
+            <img
+              className="header__btn-out"
+              src={btnOut}
+              alt="Ícone simbolizando saída/logout."
+            />
+          </Link>
+        </>
+      ) : (
+        <>
+          <div className="header__links header__links_out">
+            <NavLink className={getNavLinkClassOut} to="/">
+              Início
+            </NavLink>
+          </div>
+          <Link className="header__btn header__btn_out" to="/signin">
+            <p className="header__btn-text header__btn-text_out">Entrar</p>
+          </Link>
+        </>
+      )}
     </nav>
   );
 }
