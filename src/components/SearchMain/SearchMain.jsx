93b67c236263659e9bae3a4069153b104a41d973
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-01-19 05:39:35 -0300

    style(SearchMain): implement responsiveness in the component

--- src/components/SearchMain/SearchMain.jsx
+++ src/components/SearchMain/SearchMain.jsx
@@ -4,9 +4,7 @@ import './SearchMain.css';
 function SearchMain() {
   return (
     <section className="search main__search">
-      <h1 className="search__title">
-        O que está <br></br> acontecendo no mundo?
-      </h1>
+      <h1 className="search__title">O que está acontecendo no mundo?</h1>
       <p className="search__text">
         Encontre as últimas notícias sobre qualquer tema e salve elas em sua
         conta pessoal
