70954ec17a9693a38c007b58a60bcdf81e8ea161
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-01-24 14:10:26 -0300

    feat(utils): rename utils.js to utilsApis.js and add another utils.js file
    - utilsApis for functions and constants related to API requests
    - utils for project functions and constants

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,63 +1,10 @@
-const baseNewsApiUrl = import.meta.env.VITE_BASE_NEWS_API_URL;
-const newsApiKey = import.meta.env.VITE_NEWS_API_KEY;
-
-// Função genérica para enviar requisições HTTP
-const makeApisRequest = async ({
-  endpoint,
-  reqParams = {}, // valor padrão para evitar erro com o método URLSearchParams, caso
-  // o obj não seja passado na chamada da função, em requisições que não sejam GET
-  method,
-  headers = {}, // valor padrão para evitar erro caso não passe headers na chamada,
-  // permite que a função seja usada para requisições sem cabeçalhos (como GET simples)
-  reqBody,
-}) => {
-  try {
-    // Converte objeto em query string
-    const paramsForQuery = Object.keys(reqParams).length
-      ? new URLSearchParams(reqParams).toString()
-      : '';
-
-    const url = paramsForQuery ? `${endpoint}?${paramsForQuery}` : endpoint;
-
-    const options = {
-      headers,
-      method,
-      body: reqBody ? JSON.stringify(reqBody) : undefined,
-    };
-
-    const response = await fetch(url, options);
-
-    // O método fetch retorna o objeto de resposta no formato JSON
-    // O método res.json() converte o obj para JavaScript
-    // res é a resposta em JSON
-    // .json() converte res para JS e o return retorna os dados
-
-    const data = await response.json();
-
-    // Verifica se a solicitação foi bem ou mal sucedida
-    // Se bem, retorna o obj com dados, convertidos para JS
-    // Se mal, vai para o bloco .catch()
-
-    if (!response.ok) {
-      // Diferencia erros de rede (fetch falhou) e erros HTTP (status não OK)
-      throw new Error(`Erro HTTP ${response.status}: ${response.statusText}`);
-    }
-
-    return data;
-  } catch (error) {
-    throw new Error(`Erro na requisição para ${endpoint}: ${error.message}`); // esse bloco
-    // captura falhas do fetch (erros de rede, DNS, servidor offline) e adiciona contexto
-    // sobre qual endpoint falhou
-  }
+// Configs para prefixos dos forms: Signin e Signup
+const configSignin = {
+  formClassPrefix: '',
 };
 
-// Função para data from > reqParams, em getNews
-// getDate() → retorna o dia do mês (1 a 31) da data, pega o dia atual
-// setDate() → define o dia do mês para a data, permite alterar o dia
-const dataFromSevenDays = () => {
-  const date = new Date();
-  date.setDate(date.getDate() - 7); // subtrai 7 dias
-  return date.toISOString(); // formato ISO 8601 completo
+const configSignup = {
+  formClassPrefix: '',
 };
 
-export { baseNewsApiUrl, newsApiKey, makeApisRequest, dataFromSevenDays };
+export { configSignin, configSignup };
