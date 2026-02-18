fb3cde76d631debdc2950d5e2235a0e964bc7a98
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-02-18 12:32:40 -0300

    feat(Signup): apply handleRegistration with useFormSubmit

--- src/components/Popups/components/Signup/Signup.jsx
+++ src/components/Popups/components/Signup/Signup.jsx
@@ -1,8 +1,19 @@
+import { useContext, useState } from 'react';
+import AuthContext from '../../../../contexts/AuthContext';
+import useFormSubmit from '../../../../hooks/useFormSubmit';
 import Signin from '../Signin/Signin';
 import SignupTooltip from '../SignupTooltip/SignupTooltip';
 import './Signup.css';
 
 function Signup({ popup, handleOpenPopup, handleClosePopup }) {
+  // Variáveis de estado: controle dos inputs do formulário
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  // Contexto de autenticação, extraindo set do estado de login
+  const { handleRegistration } = useContext(AuthContext);
+
   // Objeto para configurar children de Popups para abertura do popup de login (Signin)
   // Obj duplicado, este usado aqui e em SignupTooltip
   const signinPopup = {
@@ -28,8 +39,27 @@ function Signup({ popup, handleOpenPopup, handleClosePopup }) {
     type: 'tooltip',
   };
 
+  // Envio do formulário com hook personalizado (inclui preventDefault,
+  // loading, onSubmit, onSuccess e onError)
+  const { handleSubmit } = useFormSubmit(
+    // onSubmit
+    () => {
+      // Envia informações do formulário para a API do backend (servidor) e retorna a Promisse
+      return handleRegistration({ email, password, name });
+    },
+    // onSuccess
+    () => {
+      // Abre o tooltip para msg de sucesso, que tbm é renderizado por Popup
+      handleOpenPopup(signupTooltip);
+    },
+    // onError
+    (error) => {
+      console.error('Erro ao enviar formulário de inscrição \n', error);
+    },
+  );
+
   return (
-    <form className="popup__signup">
+    <form className="popup__signup" onSubmit={handleSubmit} noValidate>
       <h2 className="popup__signup-title">Inscrever-se</h2>
       <label className="popup__signup-label" htmlFor="email">
         E-mail
@@ -39,6 +69,10 @@ function Signup({ popup, handleOpenPopup, handleClosePopup }) {
         type="email"
         placeholder="Insira e-mail"
         id="email"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
         aria-label="Inserir e-mail para cadastro"
         required
       ></input>
@@ -53,6 +87,10 @@ function Signup({ popup, handleOpenPopup, handleClosePopup }) {
         type="password"
         placeholder="Insira a senha"
         id="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
         aria-label="Inserir senha para cadastro"
         required
       ></input>
@@ -67,6 +105,10 @@ function Signup({ popup, handleOpenPopup, handleClosePopup }) {
         type="text"
         placeholder="Insira seu nome de usuário"
         id="name"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
         aria-label="Inserir seu nome de usuário"
         required
       ></input>
@@ -77,7 +119,6 @@ function Signup({ popup, handleOpenPopup, handleClosePopup }) {
       <button
         className="popup__signup-btn"
         type="submit"
-        onClick={() => handleOpenPopup(signupTooltip)}
         /*disabled*/
       >
         Inscrever-se
