8bb9e92ef7ed393d4f2eb644cc996bcb4e1ddc71
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-02-24 15:02:05 -0300

    feat(SavedNewsCardList): implement keyword ranking

--- src/components/SavedNewsCardList/SavedNewsCardList.jsx
+++ src/components/SavedNewsCardList/SavedNewsCardList.jsx
@@ -19,75 +19,139 @@ function SavedNewsCardList({ savedUserNews, memoizedHandleUnsave }) {
     );
   }
 
-  // Se houver objs de cards salvos, renderiza lista do usuário
+  // Se houverem objs de cards salvos, renderiza lista do usuário
   if (savedUserNews.userArticles.length > 0) {
     /* ----------------
-        PRIMEIRA KEY
+      PALAVRAS-CHAVE
     -----------------*/
 
-    const firstKeyword =
-      savedUserNews.userArticles[savedUserNews.userArticles.length - 1].keyword;
+    // Reduce para contabilizar qtdd de repetições de cada palavra-chave,
+    // retorna objeto com pares de propriedade(palavra-chave)/valor(qtdd)
+    const contagem = savedUserNews.userArticles.reduce((acc, item) => {
+      // Acessa a chave dentro do acumulador (a palavra-chave do item que está sendo
+      // percorrido no momento)
+      // Se a chave não existe ainda, usa 0 como valor inicial
+      acc[item.keyword] = (acc[item.keyword] || 0) + 1;
+      // Para cada item do array, devolve o acumulador atualizado - quando o reduce
+      // termina, o valor final é acc completo
+      return acc;
+      /*
+      Visualizando o processo inteiro:
+      Suponha que o array seja:
+      [{ keyword: "natureza" },  { keyword: "viagem" },  { keyword: "natureza" },  { keyword: "animais" }]
 
-    /* ----------------
-        SEGUNDA KEY
-    -----------------*/
+      Iterações:
+      Iteração 1: item.keyword = "natureza"
+                  acc = { natureza: 1 }
 
-    // Retorna array com tags diferentes da primeira
-    const diferentKey = savedUserNews.userArticles.filter((card) => {
-      return card.keyword !== firstKeyword;
+      Iteração 2: item.keyword = "viagem"
+                  acc = { natureza: 1, viagem: 1 }
+
+      Iteração 3: item.keyword = "natureza"
+                  acc = { natureza: 2, viagem: 1 }
+
+      Iteração 4: item.keyword = "animais"
+                  acc = { natureza: 2, viagem: 1, animais: 1 }
+
+      Resultado final retornado:
+      {natureza: 2,  viagem: 1,  animais: 1}
+      */
+    }, {});
+
+    // Transforma obj de retorno do reduce em array de entries, com propriedades completas
+    // tbm em array, para uso no .sort() para ordenação dos itens
+    const contagemEmEntries = Object.entries(contagem);
+
+    // Ordena palavras-chaves por qtdd de repetições, da maior para menor
+    // Passa o array com spread para ser criado um novo vetor e não alterar o original
+    // Em caso de empate, mantém a ordem original
+    const contagemOrdenada = [...contagemEmEntries].sort((a, b) => {
+      return b[1] - a[1];
     });
 
-    // Retorna o valor da segunda palavra-chave, se houver
-    // Se não houver, não quebra a aplicação (?.)
-    const secondKey = diferentKey[diferentKey.length - 1]?.keyword;
+    /* ----------------
+          KEYWORDS
+    -----------------*/
 
-    // Configura estrutura do texto para a segunda palavra-chave, de acordo com a qtdd
-    const secondKeyword = () => {
-      // Se houver 1 ou mais
-      if (diferentKey.length >= 1) {
-        return `, ${secondKey}`;
+    // Define a primeira palavra-chave
+
+    // Acessa o primeiro item do array ordenado decrescentemente (que tbm é um array) e,
+    // depois, acessa o primeiro item deste array, que é a palavra-chave com maior qtdd
+    // de repetições
+    const firstKeyword = contagemOrdenada[0][0];
+
+    // Definição para as demais palavras-chave
+
+    const defineKeywords = () => {
+      let secondKeyword;
+      let thirdKeyword;
+
+      // Se houverem três palavras-chave ou menos, exibe todas as palavras-chave
+
+      if (contagemOrdenada.length > 1 && contagemOrdenada.length <= 3) {
+        // Define a segunda palavra-chave
+        secondKeyword = contagemOrdenada[1][0];
+
+        // Define a terceira palavra-chave
+        // Se não houver a terceira, 'reseta' variável
+        contagemOrdenada.length === 3
+          ? (thirdKeyword = contagemOrdenada[2][0])
+          : '';
       }
 
-      // Se não houver nenhuma
-      return '';
+      // Se houverem mais do que três palavras-chave, exiba as primeiras duas e o número
+      // de palavras-chave restantes
+
+      if (contagemOrdenada.length > 3) {
+        // Define a segunda palavra-chave
+        secondKeyword = contagemOrdenada[1][0];
+
+        // Define a qtdd de outras no lugar da terceira palavra-chave
+        thirdKeyword = ` e ${contagemOrdenada.length - 2} outras`;
+      }
+
+      // Configura estrutura do texto para a segunda palavra-chave, de acordo com a qtdd
+      // total de palavras-chave
+      const secondKey = () => {
+        // Se não houver segunda palavra-chave
+        if (contagemOrdenada.length < 2) {
+          return '';
+        }
+
+        // Se houverem apenas duas palavras-chave
+        if (contagemOrdenada.length === 2) {
+          return ` e ${secondKeyword}`;
+        }
+
+        // Se houverem mais do que duas palavras-chave
+        if (contagemOrdenada.length > 2) {
+          return `, ${secondKeyword}`;
+        }
+      };
+
+      // Configura estrutura do texto para a terceira palavra-chave, de acordo com a qtdd
+      // total de palavras-chave
+      const thirdKey = () => {
+        // Se houver menos do que três palavras-chave
+        if (contagemOrdenada.length < 3) {
+          return '';
+        }
+
+        // Se houverem três palavras-chave
+        if (contagemOrdenada.length === 3) {
+          return ` e ${thirdKeyword}`;
+        }
+
+        // Se houverem mais do que três
+        if (contagemOrdenada.length > 3) {
+          return thirdKeyword;
+        }
+      };
+
+      return { secondKey, thirdKey };
     };
 
-    /* ----------------
-       ANOTHERS KEYS
-    -----------------*/
-
-    // Array de palavras-chave totais diferentes das tags do primeiro e segundo span,
-    // baseado no vetor resultante para a segunda palavra-chave
-    const totalAnothersKeywords = diferentKey.filter((item) => {
-      return item.keyword !== secondKey;
-    });
-
-    // Transforma totalAnothersKeywords em array contendo apenas tags
-    const totalAnothersTags = totalAnothersKeywords.map((item) => {
-      return item.keyword;
-    });
-
-    // Contabiliza apenas palavras-chave diferentes uma das outras, a partir de
-    // totalAnothersTags - se a palavra for repetida, não contabiliza
-
-    /*
-    const amountAnothersKeys = [...new Set(totalAnothersTags)].length;
-    */
-
-    // 1. Cria um Set a partir do array (remove duplicatas)
-    // 2. Transforma o Set de volta em array com [...] ou Array.from()
-    // 3. Pega o .length
-
-    const anothersTags = new Set(totalAnothersTags);
-    const anothersTagsInArray = Array.from(anothersTags);
-    const amountAnothersTags = anothersTagsInArray.length;
-
-    // Configura o texto do span de acordo com a qtdd total
-    const amountAnothersKeywords = () => {
-      return amountAnothersTags > 0
-        ? ` e ${amountAnothersTags} outra(s)`
-        : ` e 0 outra(s)`;
-    };
+    const { secondKey, thirdKey } = defineKeywords();
 
     /* ----------------
            JSX
@@ -102,13 +166,13 @@ function SavedNewsCardList({ savedUserNews, memoizedHandleUnsave }) {
             {`, você tem ${savedUserNews.userArticles.length} artigo(s) salvo(s)`}
           </p>
           <p className="saved-news__keywords">
-            Por palavras-chave:
+            Por palavra(s)-chave:
             {/* Primeira key */}
             <span className="saved-news__keyword">{` ${firstKeyword}`}</span>
             {/* Segunda key */}
-            <span className="saved-news__keyword">{`${secondKeyword()}`}</span>
+            <span className="saved-news__keyword">{`${secondKey()}`}</span>
             {/* Anothers Key */}
-            <span className="saved-news__keyword">{`${amountAnothersKeywords()}`}</span>
+            <span className="saved-news__keyword">{`${thirdKey()}`}</span>
           </p>
         </div>
         <div className="saved-news__list">
